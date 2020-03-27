6120965515e97ad89214d1bf13c2142faf42947b
Hara-castle <minanma.harajo@gmail.com>
2020-03-27 16:59:06 +0000

    validation

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,2 +1,5 @@
 class Run < ApplicationRecord
+    validates :date, presence: true, length: { maximum: 255 }
+    validates :name, presence: true, length: { maximum: 255 }
+    validates :distance, presence: true, length: { maximum: 255 }
 end
